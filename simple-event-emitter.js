2ad7b489ce871656b558837d4fce23e35da7b88f
Joakim L. Christiansen <{ID}+{username}@users.noreply.github.com>
2023-09-08 21:16:49 +0200

    .

---
+++ simple-event-emitter.js
@@ -0,0 +1,70 @@
+
+/** A bare-bones (minimal) version of Node.js EventEmitter. */
+export class EventEmitter {
+  #listenersMap = new Map()
+  
+  emit(event, ...args) {
+    const listenerMap = this.#listenersMap.get(event)
+    if (listenerMap) {
+      // setTimeout(() => {
+        for (const [listener, once] of listenerMap.entries()) {
+          if (once) this.removeEventListener(event, listener)
+          try {
+            listener(...args)
+          } catch (error) {
+            if (!this.emit('error', error)) {
+              throw error
+            }
+          }
+        }
+      // }, 0)
+      return true
+    }
+    return false
+  }
+  
+  addEventListener(event, listener, {once} = {}) {
+    this.emit('newListener', event, listener)
+    const listenerMap = this.#listenersMap.get(event)
+    if (listenerMap) {
+      listenerMap.set(listener, once)
+      for (const listener of listenerMap) {
+        listener()
+      }
+    } else {
+      const listenerMap = new Map()
+      listenerMap.set(listener, once)
+      this.#listenersMap.set(event, listenerMap)
+    }
+  }
+
+  removeEventListener(event, listener) {
+    const listenerMap = this.#listenersMap.get(event)
+    if (listenerMap) {
+      listenerMap.delete(listener)
+      this.emit('removeListener', event, listener)
+      if (!listenerMap.size) this.#listenersMap.delete(event)
+      return true
+    }
+    return false
+  }
+
+  removeAllListeners(event) {
+    if (event) this.#listenersMap.delete(event)
+    else       this.#listenersMap.clear()
+  }
+
+  on(event, listener) {
+    this.addEventListener(event, listener)
+  }
+
+  once(event, listener) {
+    this.addEventListener(event, listener, {once: true})
+  }
+
+  off(event, listener) {
+    this.removeEventListener(event, listener)
+  }
+}
+
+globalThis.EventEmitter = EventEmitter
